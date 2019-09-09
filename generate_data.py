e1aafd5c262f790feb9bd3a543380dd77677adce
FuXiuHeng <hengfuxiu@gmail.com>
2019-09-09 22:10:02 +1000

    Generate data under the prosumer model

--- generate_data.py
+++ generate_data.py
@@ -6,7 +6,9 @@ import random
 # Usage: 
 #   python3 generate_data.py <dest_name> <num_users> <num_data>
 #
-# Generate fake energy usage data with up to the given number of users and with the given number of data.
+# Generate fake energy usage data with up to the given number of uniques users and with the given number of data.
+# Each line of energy usage data will be written in the following format:
+#       <consumer_id> <energy_usage> <producer_id>
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
@@ -19,19 +21,32 @@ if __name__ == '__main__':
     num_users = int(sys.argv[2])
     num_data = int(sys.argv[3])
     
+    if num_users < 2:
+        print('Error: Must allow at least 2 distinct users')
+        exit()
+
     dest_dir_path = './data/generated'
     dest_file_path = os.path.join(dest_dir_path, '{}.txt'.format(dest_name))
     os.makedirs(dest_dir_path, exist_ok=True)
 
     f = open(dest_file_path, 'w+')
+    f.write("consumer_id energy producer_id\n")
+
+    user_id_base = 10000
+    min_total_energy = 500
+    max_total_energy = 2000
 
     for i in range(0, num_data):
-        customer_id_base = 10000
-        customer_id = random.randint(customer_id_base, customer_id_base + num_users - 1)
-        min_total_energy = 500
-        max_total_energy = 2000
+        consumer_offset = random.randint(0, num_users - 1)
+        producer_offset = random.randint(0, num_users - 1)
+        if producer_offset == consumer_offset:
+            producer_offset = (producer_offset + 1) % num_users
+        
+        consumer_id = user_id_base + consumer_offset
+        producer_id = user_id_base + producer_offset
         total_energy_usage = random.randint(min_total_energy, max_total_energy)
-        f.write('{} {}\n'.format(customer_id, total_energy_usage))
+
+        f.write('{} {} {}\n'.format(consumer_id, total_energy_usage, producer_id))
     
     f.close()
 
