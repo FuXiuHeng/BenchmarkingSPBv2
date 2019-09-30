771f45aaf0d5d5473e49cceadd4910e71c08ea09
FuXiuHeng <hengfuxiu@gmail.com>
2019-09-30 17:22:24 +1000

    Create contract for baseline

--- scripts/compile_contract.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "var energy_trade = `solc --overwrite --combined-json abi,bin,interface contracts/energy_trade.sol`" > contracts/energy_trade.js
--- settings/settings.py
+++ settings/settings.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 settings = {
     # Simulation settings
-    "num_users": 20, # Existing data has up to 85 unique users (use 86 to allow for 1 producer)
+    "num_users": 2, # Existing data has up to 85 unique users (use 86 to allow for 1 producer)
     # "simulation_name": datetime.now().strftime('%Y%m%d_%H%M_file1_70000_100000'),
     "simulation_name": datetime.now().strftime('%Y%m%d_%H%M_users_20_data_5000'),
     "energy_price": 0.00001,
