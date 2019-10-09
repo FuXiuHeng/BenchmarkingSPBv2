6ec48b9350b0724dcbb8cc28fceb954e4a348fa5
FuXiuHeng <hengfuxiu@gmail.com>
2019-10-09 22:13:48 +1100

    Added Baseline.sol smart contract

---
+++ contracts/Baseline.sol
@@ -0,0 +1,30 @@
+pragma solidity >= 0.5.0;
+
+contract Baseline {
+    address payable producer;
+    address consumer;
+    uint tradeValue;
+    uint curValue;
+
+    constructor(address _consumer, uint _value) public {
+        producer = msg.sender;
+        consumer = _consumer;
+        tradeValue = _value;
+    }
+
+    function makePayment() public payable {
+        uint value = msg.value;
+        curValue += value;
+    }
+
+    function confirmReceipt() public payable {
+        address from = msg.sender;
+        if (from == consumer) {
+            producer.transfer(tradeValue);
+        }
+    }
+
+    function isPaid() public view returns(bool) {
+        return (curValue >= tradeValue);
+    }
+}
